ddcf079e1bdda8936411bb27c97f664af12bd3ac
Albert <albert@techlifeguru.net>
2017-12-22 11:24:17 -0600

    Add video play button overlay.

---
+++ resources/js/main.js
@@ -0,0 +1,30 @@
+
+// modified from https://codepen.io/chrisnager/pen/jPrJgQ
+var videoPlayButton,
+	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+        renderVideoPlayButton: function() {
+            if (videoWrapper.contains(video)) {
+				this.formatVideoPlayButton()
+                video.classList.add('has-media-controls-hidden')
+                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
+                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+            }
+        },
+
+        formatVideoPlayButton: function() {
+            videoWrapper.insertAdjacentHTML('beforeend', '\
+                <img src="./resources/images/video-play.png" class="video-overlay-play-button" alt="Play video" />\
+								')
+        },
+
+        hideVideoPlayButton: function() {
+            video.play()
+            videoPlayButton.classList.add('is-hidden')
+            video.classList.remove('has-media-controls-hidden')
+            video.setAttribute('controls', 'controls')
+        }
+	}
+
+videoMethods.renderVideoPlayButton()
